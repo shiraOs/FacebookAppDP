fd722208ee602924424d2d4a97662834cf07d9e8
Shira Ostrinsky <shiraos@mta.ac.il>
2020-09-04 12:03:19 +0300

    adapter user-start

---
+++ C20 Ex02 Shira 311119002 Yair 305789596/C20 Ex02 Shira 311119002 Yair 305789596/AdapterUser.cs
@@ -0,0 +1,23 @@
+using FacebookWrapper.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C20_Ex02_Shira_311119002_Yair_305789596
+{
+    class AdapterUser
+    {
+        private User m_User;
+
+        public DateTime LastCheckin { get; private set; }
+
+        private void lastCheckin()
+        {
+
+        }
+
+
+    }
+}
