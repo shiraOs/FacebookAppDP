509dd4b30c1467fa12a3f6db0a35740df9181b29
yairdana <58297297+yairdana@users.noreply.github.com>
2020-10-04 12:01:59 +0300

    word

--- C20 Ex02 Shira 311119002 Yair 305789596/FacebookApp.Logic/SortListBox.cs
+++ C20 Ex02 Shira 311119002 Yair 305789596/FacebookApp.Logic/SortListBox.cs
@@ -4,25 +4,25 @@ namespace C20_Ex03_Shira_311119002_Yair_305789596
 {
     public abstract class SortListBox
     {
-        public ListBox m_List { get; set; }
+        public ListBox List { get; set; }
         public abstract bool SwapMethod(ISortableItem i_Item1, ISortableItem i_Item2);
         public void Sort()
         {
-            if (m_List.Items.Count > 1)
+            if (List.Items.Count > 1)
             {
                 bool swapped;
                 do
                 {
-                    int counter = m_List.Items.Count - 1;
+                    int counter = List.Items.Count - 1;
                     swapped = false;
 
                     while (counter > 0)
                     {
-                        if (SwapMethod(m_List.Items[counter] as ISortableItem, m_List.Items[counter - 1] as ISortableItem))
+                        if (SwapMethod(List.Items[counter] as ISortableItem, List.Items[counter - 1] as ISortableItem))
                         {
-                            object temp = m_List.Items[counter];
-                            m_List.Items[counter] = m_List.Items[counter - 1];
-                            m_List.Items[counter - 1] = temp;
+                            object temp = List.Items[counter];
+                            List.Items[counter] = List.Items[counter - 1];
+                            List.Items[counter - 1] = temp;
                             swapped = true;
                         }
                         counter--;
--- C20 Ex02 Shira 311119002 Yair 305789596/FacebookApp.Logic/SortListByName.cs
+++ C20 Ex02 Shira 311119002 Yair 305789596/FacebookApp.Logic/SortListByName.cs
@@ -6,7 +6,7 @@ namespace C20_Ex03_Shira_311119002_Yair_305789596
     {
         public SortListByName(ListBox i_List)
         {
-            m_List = i_List;
+            List = i_List;
         }
         public override bool SwapMethod(ISortableItem i_Item1, ISortableItem i_Item2)
         {
